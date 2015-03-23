43c5b5a72fc9c88844a630bf4df23666f22f8df6
Matt Shanahan <mrshanahan11235@gmail.com>
2015-03-22 19:39:51 -0500

    Removed extraneous line from parsers/__init__.py

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,4 +1,3 @@
 from glob import glob
 __all__ = map(lambda x: x.split('/')[-1][:-3], glob('parsers/*.py'))
-#__all__ = filter(lambda x: x != '__init__' and x != 'lewisshort.old', __all__)
 __all__.remove('__init__')
